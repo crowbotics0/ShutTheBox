f95c2fbf7fefc7bd5edc5a95d2028091575891f3
isc-bll <isacan0527@gmail.com>
2024-07-07 15:31:20 -0700

    added dice roller

---
+++ python_shut_the_box/Dice.py
@@ -0,0 +1,4 @@
+import random 
+
+def roll():
+    return random.randrange(1, 6)
--- python_shut_the_box/main.py
+++ python_shut_the_box/main.py
@@ -1,11 +1,27 @@
 
-print("Hello")
+import Dice
 
-number_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 flip_status_list = [False, False, False, False, False, False, False, False, False]
+game_active = True
 
-for num in range(9):
-        if flip_status_list[num] == False:
-            print(str(num + 1), end = " ")
-        else:
-            print("_", end=" ")
+while game_active:
+
+    print("~~ Current Box ~~")
+
+    for num in range(9):
+            if flip_status_list[num] == False:
+                print(str(num + 1), end = " ")
+            else:
+                print("_", end=" ")
+
+    print("\n~~ Your Dice Roll ~~")
+    dice1 = Dice.roll()
+    dice2 = Dice.roll()
+
+    print("First dice is " + str(dice1))
+    print("Second dice is " + str(dice2))
+    print("Total is " + str(dice1+ dice2))
+
+    game_active = False
+
+
