d5ebf88f41b6e5d5593c0aa8d89714a8fcc67c26
isc-bll <isacan0527@gmail.com>
2024-07-07 15:23:55 -0700

    show number status

--- python_shut_the_box/main.py
+++ python_shut_the_box/main.py
@@ -1,2 +1,11 @@
 
 print("Hello")
+
+number_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+flip_status_list = [False, False, False, False, False, False, False, False, False]
+
+for num in range(9):
+        if flip_status_list[num] == False:
+            print(str(num + 1), end = " ")
+        else:
+            print("_", end=" ")
