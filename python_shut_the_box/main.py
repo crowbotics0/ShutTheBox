6601b974a8d37005d16f2ac2173bd536c3187475
isc-bll <isacan0527@gmail.com>
2024-07-10 21:07:34 -0700

    check if addition is possible (2 nums)

--- python_shut_the_box/main.py
+++ python_shut_the_box/main.py
@@ -1,10 +1,23 @@
 
 import Dice
 
+viable_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 flip_status_list = [False, False, False, False, False, False, False, False, False]
 game_active = True
 total_flips = 0
 
+def check_addition_possibilities(dice_total):
+     if dice_total <= 9 and flip_status_list[int(dice_total) - 1] == False:
+          return True  
+     else:
+          for i in viable_numbers:
+               for j in viable_numbers:
+                    print(str(i) + " + " + str(j) + " = " + str(i+j) + ", actual: " + str(dice_total))
+                    if i + j == dice_total and i != j:
+                         print("possible")
+                         return True
+     return False
+               
 def is_move_valid(input, dice_total):
     if input.isdigit() == False:
          return False
@@ -13,7 +26,7 @@ def is_move_valid(input, dice_total):
          if num_input > 9 or num_input < 1:
               return False
          else:
-              if num_input > dice_total:
+              if num_input > dice_total or not num_input in viable_numbers:
                    return False
               else:
                    return True
@@ -28,9 +41,10 @@ def check_for_input(dice_total, total_flips):
      num_input = int(user_input)
      dice_total = dice_total - num_input
      flip_status_list[num_input - 1] = True
+     viable_numbers.remove(num_input)
      return dice_total
 
-while total_flips < 9:
+while len(viable_numbers) > 0:
     print("~~ Current Box ~~")
 
     for num in range(9):
@@ -50,10 +64,14 @@ while total_flips < 9:
 
     print("~~ Choose Number to Flip ~~")
 
-    while total > 0:
-        total = check_for_input(total, total_flips)
-        if total != 0:
-            print("Please flip another number")
+    if check_addition_possibilities(total):
+          while total > 0:
+               total = check_for_input(total, total_flips)
+               if total != 0:
+                    print("Please flip another number")
+    else:
+         print("addition not possible")
+         break
 
     game_active = False
 
